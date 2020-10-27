b2a4aa87d7c487c51e64f48192960dbec174c2bd
e-baikalova <e-baikalova@users.noreply.github.com>
2020-10-27 14:41:40 +0300

    new classes Square and Rectangle

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -4,24 +4,23 @@ public class MyFirstProgram {
 		hello("world");
 		hello("user");
 
-		double l = 5.0;  //rectangle length
-		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.l);
 
-		double a = 5.0;
-		double b = 4.0;
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + "= " + area(a, b));
+		Rectangle r = new Rectangle(4.0, 5);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 	}
 
 	public static void hello(String sText) {
 			System.out.println("Hello, " + sText + "!");
 	}
 
-	public static double area(double len) {
-		return len * len;  //rectangle square
+	public static double area(Square s) {
+		return s.l * s.l;  //square area
 	}
 
-	public static double area(double a, double b) {
-		return a * b;  //rectangle square
+	public static double area(Rectangle r) {
+		return r.a * r.b;  //rectangle area
 	}
 
 }
---
+++ sandbox/src/main/java/Square.java
@@ -0,0 +1,9 @@
+public class Square {
+
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+
+}
