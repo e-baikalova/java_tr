e08fcc2ed425a48eb3c1e871143465b3ed25a163
e-baikalova <e-baikalova@users.noreply.github.com>
2020-10-28 12:30:15 +0300

    all Points distance calculation tests passed

--- sandbox/src/test/java/PointsDistanceTests.java
+++ sandbox/src/test/java/PointsDistanceTests.java
@@ -21,13 +21,13 @@ public class PointsDistanceTests {
   public void testDistanceOnSameYCoordinatesPoints() {
     Point p1 = new Point(-1, 2);
     Point p2 = new Point(1, 2);
-    Assert.assertEquals(p1.distance(p2), 2);  //
+    Assert.assertEquals(p1.distance(p2), 2);
   }
 
   @Test
   public void testDistanceOnDifferentPoints() {
-    Point p1 = new Point(-1, 1);
-    Point p2 = new Point(3, 2);
-    Assert.assertEquals(p1.distance(p2), 5.0);  //incorrect expected value
+    Point p1 = new Point(1, 1);
+    Point p2 = new Point(5, 4);
+    Assert.assertEquals(p1.distance(p2), 5.0);
   }
 }
