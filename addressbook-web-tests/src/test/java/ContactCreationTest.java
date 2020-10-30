ea0a22aa2e494a191d3ac16ef2ba59f791d0378d
e-baikalova <e-baikalova@users.noreply.github.com>
2020-10-30 14:52:29 +0300

    Contact creation test recorded via Katalon, code was refactored

---
+++ addressbook-web-tests/src/test/java/ContactCreationTest.java
@@ -0,0 +1,95 @@
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ContactCreationTest {
+  private WebDriver wd;
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testContactCreation() throws Exception {
+    initContanctCreation();
+    fillContactForm(new ContactData("Elena", "Baikalova", "Russia, " +
+        "Moscow", "test@ts.com", "1231567"));
+    submitContactCreation();
+    returnToHomePage();
+    logout();
+  }
+
+  private void logout() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void submitContactCreation() {
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  private void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(contactData.getPhone());
+  }
+
+  private void returnToHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  private void initContanctCreation() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+}
---
+++ addressbook-web-tests/src/test/java/ContactData.java
@@ -0,0 +1,112 @@
+import java.sql.Date;
+
+public class ContactData {
+  private final String firstname;
+  private final String middlename = null;
+  private final String lastname;
+  private final String address;
+  private final String nickname = null;
+  private final String company = null;
+  private final String title = null;
+  private final String phone;
+  private final String mobileNumber = null;
+  private final String workNumber = null;
+  private final String fax = null;
+  private final String email;
+  private final String email2 = null;
+  private final String email3 = null;
+  private final String homepage = null;
+  private final Date birthsday = null;
+  private final Date anniversary = null;
+  private final String secAddress = null;
+  private final String secHome = null;
+  private final String notes = null;
+
+  public ContactData(String firstname, String lastname, String address, String email, String phone) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.address = address;
+    this.email = email;
+    this.phone = phone;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getMobileNumber() {
+    return mobileNumber;
+  }
+
+  public String getWorkNumber() {
+    return workNumber;
+  }
+
+  public String getFax() {
+    return fax;
+  }
+
+  public String getEmail2() {
+    return email2;
+  }
+
+  public String getEmail3() {
+    return email3;
+  }
+
+  public String getHomepage() {
+    return homepage;
+  }
+
+  public Date getBirthsday() { return birthsday;
+  }
+
+  public Date getAnniversary() {
+    return anniversary;
+  }
+
+  public String getSecAddress() {
+    return secAddress;
+  }
+
+  public String getSecHome() {
+    return secHome;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+}
