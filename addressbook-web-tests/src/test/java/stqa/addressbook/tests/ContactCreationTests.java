fe21e2b25bff87832bac241de1e68b8b97496451
e-baikalova <e-baikalova@users.noreply.github.com>
2020-11-30 00:15:56 +0300

    dataProvider is introduced for ContactsCreationTests

--- addressbook-web-tests/src/test/java/stqa/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/tests/ContactCreationTests.java
@@ -1,19 +1,26 @@
 package stqa.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import stqa.addressbook.model.ContactData;
 import stqa.addressbook.model.Contacts;
 import stqa.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
     String groupName = "testgroup";
     //check that group exists
     app.goTo().groupPage();
@@ -24,17 +31,32 @@ public class ContactCreationTests extends TestBase {
       //get any group name from the set
       groupName = app.group().getName();
     }
+    File photo = new File("src/test/resources/stru.png");
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(
+        new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[]{new ContactData().
+          withFirstname(split[0]).
+          withLastname(split[1]).
+          withAddress(split[2]).
+          withEmail(split[3]).
+          withPhoneNumber(split[4]).
+          withGroup(groupName)
+          //              .withPhoto(photo)
+      });
+
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
     app.goTo().homepage();
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/stru.png");
-    ContactData contact = new ContactData().
-        withFirstname("test_name2").
-        withLastname("test_lname").
-        withAddress("test address").
-        withEmail("test@email.com").
-        withPhoneNumber("1263547").
-        withGroup(groupName).
-        withPhoto(photo);
     app.contact().createContact(contact, true);
     app.goTo().homepage();
     assertThat(app.contact().count(), equalTo(before.size() + 1));
