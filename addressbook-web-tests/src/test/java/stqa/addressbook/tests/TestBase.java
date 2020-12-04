faa77a58e777f2e4a1672528bf3f04270f24778d
e-baikalova <e-baikalova@users.noreply.github.com>
2020-12-04 09:56:02 +0300

    logging is added to tests

--- addressbook-web-tests/src/test/java/stqa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/tests/TestBase.java
@@ -1,11 +1,21 @@
 package stqa.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import stqa.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
+
   //made global variable with static modificator
   protected static final ApplicationManager app
       = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
@@ -19,6 +29,16 @@ public class TestBase {
   public void tearDown() throws Exception {
 //    app.getSessionHelper().logout();
     app.stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test: " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m) {
+    logger.info("Stop test: " + m.getName());
   }
 
 }
