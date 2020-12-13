5a5df898fcdcdb83c553b9a8a7c5677d790a9e66
e-baikalova <e-baikalova@users.noreply.github.com>
2020-12-13 23:07:14 +0300

    JamesHelper implemented, no messages in inbox folder of created user

--- mantis-tests/src/test/java/stqa/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/stqa/mantis/tests/RegistrationTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import stqa.mantis.model.MailMessage;
 
+import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.List;
 
@@ -13,19 +14,21 @@ import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase{
 
-  @BeforeMethod
+//  @BeforeMethod
   public void startMailServer() {
     app.mail().start();
   }
 
   @Test
-  public void testRegistration() throws IOException {
+  public void testRegistration() throws IOException, MessagingException {
     long now = System.currentTimeMillis();
     String username = "user_" + now;
     String password = "password";
     String email = String.format("%s@localhost.localdomain", username);
+    app.james().createUser(username, password);
     app.registration().start(username, email);
-    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+//    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    List<MailMessage> mailMessages = app.james().waitForMail(username, password, 120000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
     app.registration().finish(confirmationLink, password);
     assertTrue(app.newSession().login(username, password));
@@ -37,7 +40,7 @@ public class RegistrationTests extends TestBase{
     return regex.getText(mailMessage.text);
   }
 
-  @AfterMethod(alwaysRun = true)
+//  @AfterMethod(alwaysRun = true)
   public void stopMailServer() {
     app.mail().stop();
   }
