432a8613ce2162bd33c4807e6b5ce61df9c08c32
e-baikalova <e-baikalova@users.noreply.github.com>
2020-12-25 13:29:42 +0300

    platform usage changes reverted

--- addressbook-web-tests/src/test/java/stqa/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/appmanager/ApplicationManager.java
@@ -56,7 +56,7 @@ public class ApplicationManager {
     else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7")));
+//      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7")));
       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.sever")), capabilities);
     }
 
