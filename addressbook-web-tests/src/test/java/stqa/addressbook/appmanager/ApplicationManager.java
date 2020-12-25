7b299c2aeabdde3104e85cd483d3ad972f09016c
e-baikalova <e-baikalova@users.noreply.github.com>
2020-12-25 13:09:01 +0300

    remote selenium server start is introduced

--- addressbook-web-tests/src/test/java/stqa/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/appmanager/ApplicationManager.java
@@ -1,15 +1,19 @@
 package stqa.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -36,15 +40,22 @@ public class ApplicationManager {
 
     dbHelper = new DbHelper();
 
-    if (browser.equals(BrowserType.FIREFOX)) {
-      System.setProperty("webdriver.gecko.driver", "C:\\Training\\BrowserDriver\\geckodriver.exe");
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      System.setProperty("webdriver.chrome.driver", "C:\\Training\\BrowserDriver\\chromedriver.exe");
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      System.setProperty("webdriver.ie.driver", "C:\\Training\\BrowserDriver\\IEDriverServer.exe");
-      wd = new InternetExplorerDriver();
+    if ("".equals(properties.getProperty("selenium.sever"))) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        System.setProperty("webdriver.gecko.driver", "C:\\Training\\BrowserDriver\\geckodriver.exe");
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Training\\BrowserDriver\\chromedriver.exe");
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        System.setProperty("webdriver.ie.driver", "C:\\Training\\BrowserDriver\\IEDriverServer.exe");
+        wd = new InternetExplorerDriver();
+      }
+    }
+    else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.sever")), capabilities);
     }
 
     wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
